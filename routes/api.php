84626f06145716f8efbbc7e9f4d0d01e7a1844d8
Richard Branham <richard@branham.us>
2018-01-27 17:51:54 -0500

    Moved all API calls into auth group.

--- routes/api.php
+++ routes/api.php
@@ -36,123 +36,121 @@ Route::group([
     	return $request->user();
 	});
 
-});
+	Route::get('/location', function (Request $request) {
 
+		$lat = mt_rand(33500000, 34500000) / 1000000;
+		$lng = 1 - (mt_rand(84000000, 85000000) / 1000000);
 
-Route::get('/location', function (Request $request) {
+	    $loc = new Location();
+	    $loc->latitude = $lat;
+	    $loc->longitude = $lng;
+	    $loc->users_id = 1;
+	    $loc->location_uuid = Uuid::generate();
+	    $loc->save();
 
-	$lat = mt_rand(33500000, 34500000) / 1000000;
-	$lng = 1 - (mt_rand(84000000, 85000000) / 1000000);
+		return Location::orderBy('created_at')->get();
+	});
 
-    $loc = new Location();
-    $loc->latitude = $lat;
-    $loc->longitude = $lng;
-    $loc->users_id = 1;
-    $loc->location_uuid = Uuid::generate();
-    $loc->save();
+	Route::post('/uploadfile', function (Request $request) {
+		//Log::info($request->all());
 
-	return Location::orderBy('created_at')->get();
-});
+		$timestamp = round(microtime(true));
 
+		$start = microtime(true);
 
-Route::post('/uploadfile', function (Request $request) {
-	//Log::info($request->all());
+		$diskFilePath = '/var/www/html/lds-api/storage/app/public/';
+		$diskFileName = explode(".", $request->uploads[0]->getClientOriginalName())[0] . "-$timestamp." . $request->uploads[0]->getClientOriginalExtension();
+		$diskFileNameWithPath = $diskFilePath . $diskFileName;
 
-	$timestamp = round(microtime(true));
+		$fileType = $request->uploads[0]->getMimeType();
+		Log::info("move:  " . $request->uploads[0]->move($diskFilePath, $diskFileName));
 
-	$start = microtime(true);
+		$getID3 = new getID3;
+	    $ThisFileInfo = $getID3->analyze($diskFileNameWithPath);
+	    $duration = $ThisFileInfo['playtime_seconds'];
 
-	$diskFilePath = '/var/www/html/lds-api/storage/app/public/';
-	$diskFileName = explode(".", $request->uploads[0]->getClientOriginalName())[0] . "-$timestamp." . $request->uploads[0]->getClientOriginalExtension();
-	$diskFileNameWithPath = $diskFilePath . $diskFileName;
+	    $end = microtime(true);
+	    Log::info("Processed in " . round(($end - $start) * 1000) . " milliseconds.");
 
-	$fileType = $request->uploads[0]->getMimeType();
-	Log::info("move:  " . $request->uploads[0]->move($diskFilePath, $diskFileName));
+	    $trainingModel = new TrainingContent();
+	    $trainingModel->training_contents_uuid = Uuid::generate();
+	    $trainingModel->file_path = $diskFilePath;
+	    $trainingModel->file_name = $diskFileName;
+	    $trainingModel->video_length = intval($duration);
+	    if($duration > 0) {
+	    	$trainingModel->file_type = $fileType;
+	    }
+	    $trainingModel->save();
+	});
 
-	$getID3 = new getID3;
-    $ThisFileInfo = $getID3->analyze($diskFileNameWithPath);
-    $duration = $ThisFileInfo['playtime_seconds'];
+	Route::get('/getfile', function (Request $request) {
+		$contents = Storage::get('public/StatueOfLiberty.jpg');
+		$contents = Storage::url('public/StatueOfLiberty.jpg');
+		$contents = Storage::url('public/UserStatusChanges.mp4');
+		return $contents;
+	});
 
-    $end = microtime(true);
-    Log::info("Processed in " . round(($end - $start) * 1000) . " milliseconds.");
+	Route::post('/training/getcontent', function (Request $request) {
+		Log::info("Getting content");
+		Log::info($request->all());
 
-    $trainingModel = new TrainingContent();
-    $trainingModel->training_contents_uuid = Uuid::generate();
-    $trainingModel->file_path = $diskFilePath;
-    $trainingModel->file_name = $diskFileName;
-    $trainingModel->video_length = intval($duration);
-    if($duration > 0) {
-    	$trainingModel->file_type = $fileType;
-    }
-    $trainingModel->save();
-});
-
-Route::get('/getfile', function (Request $request) {
-	$contents = Storage::get('public/StatueOfLiberty.jpg');
-	$contents = Storage::url('public/StatueOfLiberty.jpg');
-	$contents = Storage::url('public/UserStatusChanges.mp4');
-	return $contents;
-});
-
-Route::middleware('auth:api')->post('/training/getcontent', function (Request $request) {
-	Log::info("Getting content");
-	Log::info($request->all());
-
-	if(isset($request->users_id)) {
-		Log::info("users_id is set to " . $request->users_id);
-	}
-	else {
-		Log::info("users_id is not set");
-	}
-
-	$arrTrainingFiles = [];
-
-	$trainingContent = TrainingContent::all();
-
-	if(isset($trainingContent)) {
-		foreach($trainingContent as $trainingFile) {
-			$contents = ['file_path' => Storage::url("public/" . $trainingFile->file_name)];
-			$fileName = explode("-", $trainingFile->file_name)[0];
-			$fileName .= "." . explode(".", $trainingFile->file_name)[1];
-			$contents += ['file_name' => $fileName];
-			array_push($arrTrainingFiles, $contents);
+		if(isset($request->users_id)) {
+			Log::info("users_id is set to " . $request->users_id);
 		}
-	}
+		else {
+			Log::info("users_id is not set");
+		}
 
-	return $arrTrainingFiles;
-});
+		$arrTrainingFiles = [];
 
+		$trainingContent = TrainingContent::all();
 
-Route::post('/training/getprogress', function (Request $request) {
-	Log::info("Getting progress");
-	$users_id = $request->userId;
-	$training_contents_uuid = $request->contentId;
+		if(isset($trainingContent)) {
+			foreach($trainingContent as $trainingFile) {
+				$contents = ['file_path' => Storage::url("public/" . $trainingFile->file_name)];
+				$fileName = explode("-", $trainingFile->file_name)[0];
+				$fileName .= "." . explode(".", $trainingFile->file_name)[1];
+				$contents += ['file_name' => $fileName];
+				array_push($arrTrainingFiles, $contents);
+			}
+		}
 
-	Log::info("users_id = " . $users_id);
-	Log::info("training_contents_uuid = " . $training_contents_uuid);
-
-	$trainingProgress = TrainingProgress::where([['training_contents_uuid', '=', $training_contents_uuid], ['users_id', '=', $users_id]])->first();
-
-	if(!$trainingProgress) {
-		$t = new TrainingProgress();
-		$t->training_progress_uuid = Uuid::generate();
-		$t->training_contents_uuid = $training_contents_uuid;
-		$t->users_id = $users_id;
-		$t->video_last_location = 0;
-		$t->save();
-		return $t;
-	}
-
-	return $trainingProgress;
-});
+		return $arrTrainingFiles;
+	});
 
 
-Route::post('/training/updateprogress', function (Request $request) {
-	return "Nada";
+	Route::post('/training/getprogress', function (Request $request) {
+		Log::info("Getting progress");
+		$users_id = $request->userId;
+		$training_contents_uuid = $request->contentId;
 
-	$trainingProgress = TrainingContent::first();
+		Log::info("users_id = " . $users_id);
+		Log::info("training_contents_uuid = " . $training_contents_uuid);
 
-	$contents = Storage::url($trainingContent->file_path . "/" . $trainingContent->file_name);
-	return $contents;
-});
+		$trainingProgress = TrainingProgress::where([['training_contents_uuid', '=', $training_contents_uuid], ['users_id', '=', $users_id]])->first();
+
+		if(!$trainingProgress) {
+			$t = new TrainingProgress();
+			$t->training_progress_uuid = Uuid::generate();
+			$t->training_contents_uuid = $training_contents_uuid;
+			$t->users_id = $users_id;
+			$t->video_last_location = 0;
+			$t->save();
+			return $t;
+		}
+
+		return $trainingProgress;
+	});
+
+
+	Route::post('/training/updateprogress', function (Request $request) {
+		return "Nada";
+
+		$trainingProgress = TrainingContent::first();
+
+		$contents = Storage::url($trainingContent->file_path . "/" . $trainingContent->file_name);
+		return $contents;
+	});
+
+}); // group
 
